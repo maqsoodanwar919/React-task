edf1e2c1b63988fd78ff7c8ac2e246e9b6998279
maqsoodanwar919 <120628578+maqsoodanwar919@users.noreply.github.com>
2024-05-22 22:34:16 +0500

    add new project

--- crud-app/src/components/Create.jsx
+++ crud-app/src/components/Create.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+
+    const [userName, setName]= useState([]);
+    const [userAge, setAge]= useState([]);
+    const [userEmail, setUserEmail]= useState([]);
+
+    const handleSubmit = (e)=>{
+       console.log(userName, userAge,userEmail );
+    }
+
   return (
     <>
       <div className='contianer'>
         <h1 className='text-center my-4'>Create Form With React js</h1>
         <div className='row'>
            <div className='col-lg-8 me-auto ms-auto'>
-           <form  >
+           <form onSubmit={handleSubmit} >
             <div className="form-group mb-3">
                 <label className="form-label">Enter name</label>
-                <input type="text" className="form-control" placeholder='please enter name' />
+                <input type="text" className="form-control" placeholder='please enter name' name='name' value={userName} onChange={(e)=> setName(e.target.value)} />
             </div>
             <div className="form-group mb-3">
                 <label className="form-label">Enter Age</label>
-                <input type="number" className="form-control" placeholder='please enter age' />
+                <input type="number" className="form-control" placeholder='please enter age'  name='age' value={userAge}  onChange={(e)=> setAge(e.target.value)}/>
             </div> 
             <div className="form-group mb-3">
                 <label className="form-label">Enter Email</label>
-                <input type="email" className="form-control"  placeholder='please enter email'/>
+                <input type="email" className="form-control"  placeholder='please enter email' name='email' value={userEmail}  onChange={(e)=> setUserEmail(e.target.value)}/>
             </div> 
             <div className="form-group">
-                <input type='button' className="btn btn-primary" value='Submit form'/> 
+                <input type='submit' className="btn btn-primary" value='Submit form'/> 
             </div>
             </form>
            </div>
