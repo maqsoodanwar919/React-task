46b9b5838b28636839fec1383f2e56fb90213bde
maqsoodanwar919 <120628578+maqsoodanwar919@users.noreply.github.com>
2024-06-03 20:14:09 +0500

    add crud

--- crud_app/src/Components/Pages/Edit.jsx
+++ crud_app/src/Components/Pages/Edit.jsx
@@ -31,7 +31,7 @@ export const Edit = () => {
 
     return (
         <div className='container'>
-        <h1 className='text-center my-4'>Edit User</h1>
+        <h1 className='text-center my-4'>Edit {userName}</h1>
         <div className='row'>
             <div className='col-lg-12 me-auto ms-auto'>
                 <Link to="/read">
